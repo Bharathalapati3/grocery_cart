acbdb256b36d731bad3e0ee1a6e66a4af4a29008
Bharathalapati3 <bharathalapati03@gmail.com>
2019-10-03 05:06:49 +0530

    Updates

---
+++ precache-manifest.4801c05c50bbfceb5e7501e048ab9d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce9abf2e12a60bd118de59fcb199a7e",
+    "url": "/grocery_cart/index.html"
+  },
+  {
+    "revision": "5866d17a44969a9eade6",
+    "url": "/grocery_cart/static/css/main.1f63a806.chunk.css"
+  },
+  {
+    "revision": "bfb562cda0ff4a702729",
+    "url": "/grocery_cart/static/js/2.616d7c8f.chunk.js"
+  },
+  {
+    "revision": "5866d17a44969a9eade6",
+    "url": "/grocery_cart/static/js/main.57097fa5.chunk.js"
+  },
+  {
+    "revision": "72f48155c36d0ab55446",
+    "url": "/grocery_cart/static/js/runtime-main.ae1de994.js"
+  }
+]);
